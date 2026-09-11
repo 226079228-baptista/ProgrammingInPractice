3d106694dfb421220fb2ec16bbdc4129b0ffa915
Liliana Baptista <baptistaliliana90@gmail.com>
2026-09-11 14:54:41 +0200

    basic municipality display

--- Week1/main.c
+++ Week1/main.c
@@ -1,43 +1,10 @@
 #include <stdio.h>
-int budget (){
-
-double revenue;
-double expenses;
-double balance;
-
-printf("MUNICIPAL BUDGET CALCULATOR\n");
-printf("---------------------------\n");
-
-printf("Enter total revenue: ");
-scanf("%lf", &revenue);
-
-printf("Enter total expenses: ");
-scanf("%lf", &expenses);
-
-balance = revenue - expenses;
-
-printf("\nRevenue: %.2f\n", revenue);
-printf("Expenses: %.2f\n", expenses);
-
-if (balance > 0) {
-printf("Surplus: %.2f\n", balance);
-}
-else if (balance < 0) {
-printf("Deficit: %.2f\n", -balance);
-}
-else {
-printf("The budget is balanced.\n");
-}
-return 0;
-
-}
 
 int main() {
 
 char municipality[50];
 char mayor[50];
 int population;
-int callbudget;
 
 printf("Welcome to Windhoek Municipality\n\n");
 
@@ -55,11 +22,6 @@ printf("Municipality : %s\n", municipality);
 printf("Mayor: %s\n", mayor);
 printf("Population : %d\n", population);
 
-printf("Press 1 if you want do budget\n");
-scanf("%d", &callbudget);
-if ( callbudget == 1) {
-budget();
-}
 
 return 0;
 }
