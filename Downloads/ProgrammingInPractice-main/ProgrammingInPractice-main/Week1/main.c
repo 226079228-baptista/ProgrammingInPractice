bbe06fbd4f1757cb4b567d6322792e9b4b234709
Liliana Baptista <baptistaliliana90@gmail.com>
2026-09-16 11:46:13 +0200

    Week 1

--- Downloads/ProgrammingInPractice-main/ProgrammingInPractice-main/Week1/main.c
+++ Downloads/ProgrammingInPractice-main/ProgrammingInPractice-main/Week1/main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
+
+
+int budget (){
+
+double revenue;
+double expenses;
+double balance;
+
+printf("MUNICIPAL BUDGET CALCULATOR\n");
+printf("---------------------------\n");
+
+printf("Enter total revenue: ");
+scanf("%lf", &revenue);
+
+printf("Enter total expenses: ");
+scanf("%lf", &expenses);
+
+balance = revenue - expenses;
+
+printf("\nRevenue: %.2f\n", revenue);
+printf("Expenses: %.2f\n", expenses);
+
+if (balance > 0) {
+printf("Surplus: %.2f\n", balance);
+}
+else if (balance < 0) {
+printf("Deficit: %.2f\n", -balance);
+}
+else {
+printf("The budget is balanced.\n");
+}
+return 0;
+
+}
+
 int main() {
+
 char municipality[50];
 char mayor[50];
 int population;
+int callbudget;
 
 printf("Welcome to Windhoek Municipality\n\n");
 
@@ -20,6 +57,12 @@ printf("Municipality : %s\n", municipality);
 printf("Mayor: %s\n", mayor);
 printf("Population : %d\n", population);
 
+printf("Press 1 if you want to call budget\n");
+scanf("%d", &callbudget);
+if ( callbudget == 1) {
+budget();
+}
 
 return 0;
 }
+
